5a3d6c2c342524f07c71b9cc63f84a36d0227b59
hafeezullah99 <hafeez.ullah@joybox.pk>
2024-06-16 16:18:43 +0500

    updated the api

--- lib/views/splash_screen.dart
+++ lib/views/splash_screen.dart
@@ -1,8 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
 import 'package:rocki_poin_app/core/constants/app_assets.dart';
 import 'package:rocki_poin_app/core/constants/app_colors.dart';
+import 'package:rocki_poin_app/views/user_details/model/provider/user_provider.dart';
+import 'package:rocki_poin_app/views/user_details/model/user_model.dart';
 import 'package:rocki_poin_app/views/welcome_bonus/welcome_bonus_screen.dart';
+import 'package:telegram_web_app/telegram_web_app.dart';
 
 import '../core/constants/app_strings.dart';
 
@@ -16,11 +21,47 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  bool? isDefinedVersion;
+  String? telegramUsername;
+
+  final TelegramWebApp telegram = TelegramWebApp.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  void check() async {
+    await Future.delayed(const Duration(seconds: 2));
+    isDefinedVersion = await telegram.isVersionAtLeast('Bot API 6.1');
+    telegram.ready();
+
+    String userId = telegram.initData.user?.id.toString() ?? 'Unknown ID';
+    String username = telegram.initDataUnsafe?.user?.first_name ?? 'User name';
+    String userImageUrl = telegram.initDataUnsafe!.user?.photo_url ??
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRKgUUpHpc-JwcJiRLScAepL-T3oeaxR8T5A&s';
+
+    // Save the user details to Firestore
+    await _firestore.collection('users').doc(userId).set({
+      'id': userId,
+      'firstName': username,
+      'imageUrl': telegram.initDataUnsafe!.user?.photo_url,
+    });
+
+    // _logger.info('Username: $username'); // Log using the logging package
+
+    Provider.of<UserProvider>(context, listen: false).setUser(
+      UserModel(id: userId, firstName: username, imageUrl: userImageUrl),
+    );
+    // Update the state with the fetched username
+    setState(() {
+      telegramUsername = username;
+    });
+  }
+
   @override
   void initState() {
     super.initState();
+    TelegramWebApp.instance.ready();
+    check();
     Future.delayed(
-        Duration(seconds: 4),
+        const Duration(seconds: 4),
         () => Navigator.pushReplacementNamed(
             context, WelcomeBonusScreen.routeName));
   }
--- lib/views/user_details/model/user_model.dart
+++ lib/views/user_details/model/user_model.dart
@@ -1,38 +1,8 @@
-class User {
-  final String name;
-  final String username;
-  final String email;
+// user_model.dart
+class UserModel {
+  final String id;
+  final String firstName;
   final String imageUrl;
-  final int level;
-  final int coin;
 
-  User({
-    required this.name,
-    required this.username,
-    required this.email,
-    required this.imageUrl,
-    required this.level,
-    required this.coin,
-  });
-
-  factory User.fromMap(Map<String, dynamic> data, String email) {
-    return User(
-      name: data['name'],
-      username: data['username'],
-      email: email,
-      imageUrl: data['image_url'],
-      level: data['level'],
-      coin: data['coin'],
-    );
-  }
-
-  Map<String, dynamic> toMap() {
-    return {
-      'name': name,
-      'username': username,
-      'image_url': imageUrl,
-      'level': level,
-      'coin': coin,
-    };
-  }
+  UserModel({required this.id, required this.firstName, required this.imageUrl});
 }
